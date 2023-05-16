b0a41a173cc8d90a4ec70135502ad158d7f80374
awais-rafiq <124677568+awais-rafiq@users.noreply.github.com>
2023-05-16 05:11:01 -0700

    Add files via upload

---
+++ objects programms/dom.js
@@ -0,0 +1,87 @@
+// how can access html element using dom in javascript
+
+// document
+
+// let mydocument=document;
+// myElement.text.document="newtext content";
+// myElement.addEvenListner('click',function()){
+//     // your code to exectuewhen elemnt is clicked
+// }
+
+// if  multiple element  with a
+// if  multiple element  with spcific class name ;
+// querySelectorAll()
+// 'getElement by classname()' instead if getElemntbyid
+// get element by Id
+// unique attribute
+// let heading =document.getElementById("heading1")
+// heading.innerHTML = "Awais love Pakistan"
+// let button = document.getElementById("btn")
+// button.addEventListener("click",()=>{
+//     alert("YOU CLICKED ME!")
+// })
+// let changeHeading = document.getElementsByClassName("heading")
+// changeHeading.innerHTML = "Mazhar Siddiqi"
+
+// let head = document.getElementsByClassName("head")
+// head.innerTEXT ="Heading 2"
+// let headings=document.getElementsByClassName("heading")
+// headings[0].style.color="red";
+// headings[1].style.backgroundColor="green";
+// headings[1].style.border="2px red solid";
+// headings[1].style.padding ="4px 10px";
+
+
+// console.log(headings)
+
+// headings[0].style.color = "blue"
+// headings[0].style.border = "1px solid black"
+// headings[1].style.border = "1px solid black"
+// headings[1].style.padding = "20px 50px"
+
+
+
+
+
+
+
+
+
+
+
+
+
+// what are the activities that can perform using DOM
+
+// accessing element
+// let document=document.getElementById
+
+// let element= element.getElemenByClassName("my class")
+
+// let element= element.getElemenByTagName("div")
+
+// change the Style
+
+
+// event hsndling
+// element.addEventListener("click",function(){
+
+// })
+
+// remove an event listner from anelement
+
+
+// transverng the DOM
+// let parent=element.parentNode;
+
+
+// accesing the next subling  of node 
+
+// let sibling=element.nextElementSibling;
+
+
+
+// accesing the first child elent of Node 
+
+
+// let child = element.firstElementChild;
